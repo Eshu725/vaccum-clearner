e9889f0bdc044a9b9313201f9fee6cbf9be3d9a0
lifetheory <66261091+Eshu725@users.noreply.github.com>
2020-07-18 20:03:41 +0530

    Update vaccum-cleaner.cpp

--- vaccum-cleaner.cpp
+++ vaccum-cleaner.cpp
@@ -1,12 +1,6 @@
-/ Welcome to Vacuum Cleaner World.  
+// Welcome to Vacuum Cleaner World.  
 
-// This program constructs a 2D grid world, fills it with dirt, then
-// runs an intelligent (or un- as the case may be) vacuum cleaner
-// through it to clean up.
 
-// Well, in theory, anyway.  Currently, the agent will read in the commands,
-// but not carry them out.  Other than that one (admittedly large) mixup,
-// this program runs cleanly.
 
 #include <iostream>
 #include <stdlib.h>     /* srand, rand */
@@ -15,12 +9,7 @@
 using namespace std;
 
 
-//The basic agent.  It's aware of its x and y positions (even if it
-//chooses to ignore this information), and direction (0 is north, 1 is
-//east, 2 south, 3 west, 4 up, 5 down).  It keeps track of its score
-//(how well it meets expected goals), and its type determines how it
-//interacts with the world and gives some information on when to
-//change score.
+
 
 struct agent {
         int type;
@@ -32,18 +21,17 @@ struct agent {
 
 class world {
 private:
-        int height;     //The world is a 2D grid - hence the height and width
+        int height;     
         int width;
 public:
         int contents[4][4];     //make a 2D array to hold dirt
 
-// Pick a random spot on the grid and fill it with dirt.
+
         inline void create_dirt () {
                 contents[rand()%4][rand()%4] = 1;
         }
 
-// Initializes the grid.  Sets height and weight to 4 and makes sure
-// the grid is empty.
+
         void initialize() {
                 height = 4;
                 width = 4;
@@ -63,7 +51,7 @@ void display() {
 						cout<<endl;
                 }
         }
-// This brings up a display of the grid and reports the agent's location
+
         void report_dims (agent x) {
                 for (int i=0;i<height;i++) {
                         for (int j=0;j<width;j++) {
@@ -75,8 +63,7 @@ void display() {
                         << x.direction << " with score " << x.score << "\n";
         }
 
-// Agent functions.  Report state of current square, turn left or
-// right, move forward
+
         inline int report (agent x) {
                 return (contents[x.ypos][x.xpos]);
         }
@@ -103,15 +90,9 @@ void display() {
                 }
         }
 
-// Suck up dirt in the case of a vacuum, though could apply to
-// anything eating anything else later
+
         inline void eat_here (agent x) {
-//      This is where I deal with scoring based on the agent's type
-//              switch (x.type) {
-//              case 2:
-//                      if (contents[x.ypos][x.xpos] = 1)
-//                              x.score += 100;
-//              }
+
                 contents[x.ypos][x.xpos] = 0;
         }
 };
@@ -137,9 +118,7 @@ void run_rand_vac(agent x, world y) {
         }
 }
 
-//--A single move for an intelligent agent--// If report returns
-//something other than 0, that means there's something to eat.  If
-//there's nothing to eat, make a random move.
+
 void run_smart_vac(agent x, world y) {
         if (y.report(x)) 
                 y.eat_here(x); else
